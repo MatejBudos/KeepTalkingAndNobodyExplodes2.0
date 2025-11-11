f5ba7fee792d5001be09a0afe2b3b4f815e88dcb
budos <budos2@uniba.sk>
2025-11-11 19:01:04 +0100

    add timer

---
+++ KeepTalking-UnityApp/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingTime < 10)
+        {
+            timerText.color = Color.red;
+        }
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else
+        {
+            remainingTime = 0;
+        }
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);   
+    }
+}
